50c32a77c83ba811afb88e3f171f4a648fe388e2
David Greenwald <david@davidgreenwald.com>
2018-02-18 13:19:37 -0800

    Set variable for regex image files pattern, minor regex refactor

--- delete-webp.php
+++ delete-webp.php
@@ -26,8 +26,10 @@ Author URI: http://www.davidgreenwald.com
  	if( isset( $attachmentid ) ) {
  		// get the file path for the image being deleted
  		$img = get_attached_file( $attachmentid );
+		// set the image pattern for regex use
+		$img_formats = '/\.(jp(e)?g|png|gif)/';
  		//get the file path without the .jpg, .png, or .gif extension
- 		$img_root = preg_replace( '/\.jp(e)?g|\.png|\.gif/', '', $img );
+ 		$img_root = preg_replace( $img_formats, '', $img );
  		// look for .webp files matching the path, including all thumbnails!
  		$webp_files = glob( $img_root . '*.webp' );
 
@@ -41,7 +43,7 @@ Author URI: http://www.davidgreenwald.com
 		 */
 
 		// Run preg_match on the original image before entering the loop
-		preg_match( '/\.jp(e)?g|\.png|\.gif/', $img, $img_matches);
+		preg_match( $img_formats, $img, $img_matches);
 
  		// do a foreach loop to delete each .webp image for original and thumbnails
  		foreach( $webp_files as $webp_file ) {
@@ -50,18 +52,15 @@ Author URI: http://www.davidgreenwald.com
 				// match for the .webp file's original extension
 				// Preg Match returns 1, 0 or false on error
 				// @link http://php.net/manual/en/function.preg-match.php
-				$webp_match_test = preg_match( '/\.jp(e)?g|\.png|\.gif/', $webp_file, $webp_matches);
+				$webp_match_test = preg_match( $img_formats, $webp_file, $webp_matches);
 				// if no match, we have img.webp files - safe to delete
 				// or if extensions match, only delete .webp from the correct parent
-				if(
-					$webp_match_test === 0
-					||
-					($webp_match_test === 1 && $webp_matches === $img_matches)
-				) {
+				if( $webp_match_test === 0 ||
+					($webp_match_test === 1 && $webp_matches === $img_matches) ) {
 					wp_delete_file( $webp_file );
-				}
- 			 } // end if(file_exists)
- 		 } // end foreach
+				} // end if match test
+ 			} // end if(file_exists)
+ 		} // end foreach
  	} // end if(isset)
  } // end function
  add_action( 'delete_attachment', 'adwf_delete_webp_with_original_image' );
